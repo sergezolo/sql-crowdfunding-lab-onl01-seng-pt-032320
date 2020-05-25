c3812b2ee3870d644ddbf10c3895a18c80316301
sergezolo <zolotukhinsereg@gmail.com>
2020-05-25 17:56:19 -0500

    Done.

---
+++ lib/insert.sql
@@ -0,0 +1,66 @@
+INSERT INTO projects (id) VALUES (1);
+INSERT INTO projects (id) VALUES (2);
+INSERT INTO projects (id) VALUES (3);
+INSERT INTO projects (id) VALUES (4);
+INSERT INTO projects (id) VALUES (5);
+INSERT INTO projects (id) VALUES (6);
+INSERT INTO projects (id) VALUES (7);
+INSERT INTO projects (id) VALUES (8);
+INSERT INTO projects (id) VALUES (9);
+INSERT INTO projects (id) VALUES (10);
+
+INSERT INTO users (id) VALUES (1);
+INSERT INTO users (id) VALUES (2);
+INSERT INTO users (id) VALUES (3);
+INSERT INTO users (id) VALUES (4);
+INSERT INTO users (id) VALUES (5);
+INSERT INTO users (id) VALUES (6);
+INSERT INTO users (id) VALUES (7);
+INSERT INTO users (id) VALUES (8);
+INSERT INTO users (id) VALUES (9);
+INSERT INTO users (id) VALUES (10);
+INSERT INTO users (id) VALUES (11);
+INSERT INTO users (id) VALUES (12);
+INSERT INTO users (id) VALUES (13);
+INSERT INTO users (id) VALUES (14);
+INSERT INTO users (id) VALUES (15);
+INSERT INTO users (id) VALUES (16);
+INSERT INTO users (id) VALUES (17);
+INSERT INTO users (id) VALUES (18);
+INSERT INTO users (id) VALUES (19);
+INSERT INTO users (id) VALUES (20);
+
+
+
+INSERT INTO pledges (id) VALUES (1);
+INSERT INTO pledges (id) VALUES (2);
+INSERT INTO pledges (id) VALUES (3);
+INSERT INTO pledges (id) VALUES (4);
+INSERT INTO pledges (id) VALUES (5);
+INSERT INTO pledges (id) VALUES (6);
+INSERT INTO pledges (id) VALUES (7);
+INSERT INTO pledges (id) VALUES (8);
+INSERT INTO pledges (id) VALUES (9);
+INSERT INTO pledges (id) VALUES (10);
+INSERT INTO pledges (id) VALUES (11);
+INSERT INTO pledges (id) VALUES (12);
+INSERT INTO pledges (id) VALUES (13);
+INSERT INTO pledges (id) VALUES (14);
+INSERT INTO pledges (id) VALUES (15);
+INSERT INTO pledges (id) VALUES (16);
+INSERT INTO pledges (id) VALUES (17);
+INSERT INTO pledges (id) VALUES (18);
+INSERT INTO pledges (id) VALUES (19);
+INSERT INTO pledges (id) VALUES (20);
+INSERT INTO pledges (id) VALUES (21);
+INSERT INTO pledges (id) VALUES (22);
+INSERT INTO pledges (id) VALUES (23);
+INSERT INTO pledges (id) VALUES (24);
+INSERT INTO pledges (id) VALUES (25);
+INSERT INTO pledges (id) VALUES (26);
+INSERT INTO pledges (id) VALUES (27);
+INSERT INTO pledges (id) VALUES (28);
+INSERT INTO pledges (id) VALUES (29);
+INSERT INTO pledges (id) VALUES (30);
+
+
